ddaf4dfb45893c7ae9cd16e52a541008c06c23fc
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-12-03 21:08:39 +0000

    Semi-working table creation

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Schema
 
-Schema allows you to define your database schema in pure, beautiful PHP. No more feeding the DBForge library raw SQL fragments or dumping and loading at the command line. Schema lets you design your database in your migrations with minimal ease.
+Schema allows you to write your migrations (and define your database tables) in pure, beautiful PHP. No more feeding the DBForge library raw SQL fragments or dumping and loading at the command line. Schema lets you design your database in your migrations with ultimate ease.
 
 # Synopsis
 
 ```php
-Schema::create_table(function($table){
+require_once 'libraries/Schema.php';
+
+Schema::create_table('users', function($table){
     
     $table->auto_increment_integer('id');
     
---
+++ libraries/Schema.php
@@ -0,0 +1,158 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Schema
+ *
+ * Expressive table definitions
+ *
+ * @author 		Jamie Rumbelow <http://jamierumbelow.net>
+ * @version		0.0.1.alpha
+ * @copyright 	(c)2011 Jamie Rumbelow
+ */
+
+/* --------------------------------------------------------------
+ * THE SCHEMA FACTORY CLASS
+ * ------------------------------------------------------------ */
+ 
+class Schema {
+    
+    /* --------------------------------------------------------------
+	 * VARIABLES
+	 * ------------------------------------------------------------ */
+    
+    /* --------------------------------------------------------------
+	 * GENERIC METHODS
+	 * ------------------------------------------------------------ */
+	 
+	public function __construct() { }
+	
+	/* --------------------------------------------------------------
+	 * FACTORY API
+	 * ------------------------------------------------------------ */
+    
+    static public function create_table($table_name, $callback) {
+        $table_definition = new Schema_Table_Definition($table_name);
+        $callback($table_definition);
+        
+        $ci =& get_instance();
+        $ci->load->dbforge();
+        
+        $ci->dbforge->add_field($table_definition->columns());
+        
+        foreach ($table_definition->keys() as $key => $primary) {
+            $ci->dbforge->add_key($key, $primary);
+        }
+        
+        $ci->dbforge->create_table($table_definition->table_name());
+    }
+    
+}
+
+/* --------------------------------------------------------------
+ * SCHEMA TABLE DEFINITION CLASS
+ * ------------------------------------------------------------ */
+
+class Schema_Table_Definition {
+
+    /* --------------------------------------------------------------
+	 * VARIABLES
+	 * ------------------------------------------------------------ */
+    
+    protected $name = '';
+    protected $definition = array(
+        'columns' => array(),
+        'keys' => array()
+    );
+    
+    /* --------------------------------------------------------------
+	 * GENERIC METHODS
+	 * ------------------------------------------------------------ */
+
+    public function __construct($table_name = '') {
+        $this->name = $table_name;
+    }
+
+    /* --------------------------------------------------------------
+	 * GENERIC API
+	 * ------------------------------------------------------------ */
+    
+    public function columns() {
+        return $this->definition['columns'];
+    }
+    
+    public function keys() {
+        return $this->definition['keys'];
+    }
+    
+    public function table_name() {
+        return $this->name;
+    }
+    
+    /* --------------------------------------------------------------
+	 * COLUMN API
+	 * ------------------------------------------------------------ */
+    
+    public function integer($column_name, $options = array()) {
+        $this->_add_definition_rule($column_name, array(
+            'type' => 'INT'
+        ), $options);
+    }
+    
+    public function auto_increment_integer($column_name, $options = array()) {
+        $this->integer($column_name, array_merge(array(
+            'unsigned' => TRUE,
+            'auto_increment' => TRUE
+        ), $options));
+        
+        $this->key($column_name, TRUE);
+    }
+    
+    public function string($column_name, $constraint = 200, $options = array()) {
+        $this->_add_definition_rule($column_name, array(
+            'type' => 'VARCHAR',
+            'constraint' => $constraint
+        ), $options);
+    }
+    
+    public function text($column_name, $options = array()) {
+        $this->_add_definition_rule($column_name, array(
+            'type' => 'TEXT'
+        ), $options);
+    }
+    
+    public function date($column_name, $options = array()) {
+        $this->_add_definition_rule($column_name, array(
+            'type' => 'DATE'
+        ), $options);
+    }
+    
+    public function datetime($column_name, $options = array()) {
+        $this->_add_definition_rule($column_name, array(
+            'type' => 'DATETIME'
+        ), $options);
+    }
+    
+    public function timestamps() {
+        $this->datetime('created_at', array(''));
+    }
+    
+    /* --------------------------------------------------------------
+	 * MISC API
+	 * ------------------------------------------------------------ */
+    
+    public function primary_key($column_name) {
+        $this->key($column_name, TRUE);
+    }
+    
+    public function key($column_name, $primary = FALSE) {
+        $this->definition['keys'][$column_name] = $primary;
+    }
+    
+    /* --------------------------------------------------------------
+	 * HELPER METHODS
+	 * ------------------------------------------------------------ */
+    
+    protected function _add_definition_rule($column_name, $rule, $options) {
+        $this->definition['columns'][$column_name] = array_merge($rule, $options);
+    }
+    
+}
