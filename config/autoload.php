85897a13313ef18e5b42b34fb81de7aeff895060
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-12-10 15:22:27 +0000

    Autoload the Schema library as a part of the spark; it's the only thing in it

---
+++ config/autoload.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Schema
+ *
+ * Expressive table definitions
+ *
+ * @author 		Jamie Rumbelow <http://jamierumbelow.net>
+ * @version		0.1.0
+ * @copyright 	(c)2011 Jamie Rumbelow
+ */
+ 
+$autoload = array();
+$autoload['libraries'] = array('schema');
